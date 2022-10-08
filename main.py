ddd174c89068c8148c9d36b92fd1c100191a63be
Lucas Pains <jlucaspains@gmail.com>
2022-10-07 17:17:51 -0700

    Added cors

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-import uuid
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from logging.handlers import RotatingFileHandler
 from pydantic import BaseModel
 from recipe_scrapers import scrape_me
 import re
+import uuid
+import time
 from fractions import Fraction
 from pint import UnitRegistry
 import logging
-import time
-from logging.handlers import RotatingFileHandler
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "https://app.sharpcooking.net",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class RecipeIngredient(BaseModel):
     raw: str
